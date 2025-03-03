0e97f42768161dd0a5f0cdffb4a496c03e7481a1
Tal Jacob <taljacob2@gmail.com>
2025-03-03 10:25:46 +0200

    Reorder `uploadImage` Return Value For TypeError
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- insighthub-backend/src/services/resources_service.ts
+++ insighthub-backend/src/services/resources_service.ts
@@ -36,7 +36,7 @@ const createImagesStorage = () => {
             if (extname && mimetype) {
                 return cb(null, true);
             } else {
-                cb(new TypeError(`Invalid file type. Only images are allowed: ${allowedTypes}.`));
+                return cb(new TypeError(`Invalid file type. Only images are allowed: ${allowedTypes}.`));
             }
         }
     });
