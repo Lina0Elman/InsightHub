f9456f62dd9d68c62eac2c7344c77e6abcc314b6
Tal Jacob <taljacob2@gmail.com>
2025-03-04 09:52:20 +0200

    Add Basic List For Getting All Posts
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- insighthub-frontend/src/pages/Dashboard.tsx
+++ insighthub-frontend/src/pages/Dashboard.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
-import { Container, Typography, Box, Button } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Container, Typography, Box, Button, List, ListItem, ListItemText, Divider } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { config } from '../config';
 
 const Dashboard: React.FC = () => {
   const navigate = useNavigate();
+  const [posts, setPosts] = useState<any[]>([]);
+  const accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2FmYTc0MDY4ZjczNmYxMTJhZTFkNTEiLCJyYW5kb20iOjEyNzEsImlhdCI6MTc0MTA3NDQ3NSwiZXhwIjoxNzQxMDc4MDc1fQ.VQqlhKMY_Wt2rh4OussLNR0euFqQTpT345KLe5qNkj4'; // Replace with your actual access token
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get(`${config.app.backend_url()}/post`, {
+          headers: {
+            Authorization: encodeURI(`Bearer ${accessToken}`),
+          },
+        });
+        setPosts(response.data as []);
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
 
   const handleLogout = () => {
     // Clear any authentication tokens or user data here
@@ -22,6 +43,38 @@ const Dashboard: React.FC = () => {
         <Button variant="contained" color="primary" onClick={handleLogout}>
           Logout
         </Button>
+        <List sx={{ width: '100%', mt: 4 }}>
+          {posts.map((post) => (
+            <React.Fragment key={post._id}>
+              <ListItem alignItems="flex-start">
+                <ListItemText
+                  primary={post.title}
+                  secondary={
+                    <React.Fragment>
+                      <Typography
+                        component="span"
+                        variant="body2"
+                        color="text.primary"
+                      >
+                        {post.sender}
+                      </Typography>
+                      {" — " + post.content}
+                    </React.Fragment>
+                  }
+                />
+              </ListItem>
+              {post.comments && post.comments.map((comment: any) => (
+                <ListItem key={comment._id} sx={{ pl: 4 }}>
+                  <ListItemText
+                    primary={comment.sender}
+                    secondary={comment.content}
+                  />
+                </ListItem>
+              ))}
+              <Divider component="li" />
+            </React.Fragment>
+          ))}
+        </List>
       </Box>
     </Container>
   );
