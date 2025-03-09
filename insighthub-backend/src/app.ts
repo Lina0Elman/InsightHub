57a3fcad5c28f3cd33308edfb6ba955cee18199b
LiavTB <49226046+LiavTB@users.noreply.github.com>
2025-03-10 01:21:34 +0200

    Add cors to the server for the client

--- insighthub-backend/src/app.ts
+++ insighthub-backend/src/app.ts
@@ -8,10 +8,18 @@ import swaggerJsdoc from 'swagger-jsdoc';
 import options from './docs/swagger_options';
 import {authenticateToken, authenticateTokenForParams} from "./middleware/auth";
 import bodyParser from 'body-parser';
+import cors from 'cors';
+import {config} from "./config/config";
 
 const specs = swaggerJsdoc(options);
 
 const app = express();
+
+app.use(cors({
+    origin: [config.app.frontend_url(), config.app.backend_url()],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Allow cookies to be sent with requests
+}));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
