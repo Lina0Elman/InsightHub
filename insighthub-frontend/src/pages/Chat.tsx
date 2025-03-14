226324c02e56784d0de240541bcf73da851e157f
Tal Jacob <taljacob2@gmail.com>
2025-03-14 17:35:54 +0200

    Add Client Authentication For Socket.io
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- insighthub-frontend/src/pages/Chat.tsx
+++ insighthub-frontend/src/pages/Chat.tsx
@@ -7,9 +7,16 @@ const Chat: React.FC = () => {
   const [message, setMessage] = useState('');
   const [allMessages, setAllMessages] = useState([] as string[]);
   const socketRef = useRef(null as any);
+
+  // TODO: Replace this with accessToken from localstorage
+  const accessTokenRef = useRef("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2FmYTc0MDY4ZjczNmYxMTJhZTFkNTEiLCJyYW5kb20iOjYxOTEyNSwiaWF0IjoxNzQxOTY1MDA0LCJleHAiOjE3NDE5Njg2MDR9.Bo6j8MqBr1FCLIJm6BJ73PZGGC6Xo8bHYDB_JhA9P6Y");
   
   const connectHandler = () => {
-    socketRef.current = io(config.app.backend_url());
+    socketRef.current = io(config.app.backend_url(), {
+      extraHeaders: {
+        authorization: `Bearer ${accessTokenRef.current}`
+      }
+    });
     socketRef.current.on(config.socketMethods.messageFromServer, (receivedMessage: string) => {
       setAllMessages((allMessages) => [...allMessages, receivedMessage]);
     });
