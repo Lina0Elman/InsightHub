4a22a163aaee126d60280c5380fc0e77f21d6f4d
Tal Jacob <taljacob2@gmail.com>
2025-03-16 01:19:16 +0200

    See Selected User In List
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- insighthub-frontend/src/components/DividedList.tsx
+++ insighthub-frontend/src/components/DividedList.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Avatar from '@mui/joy/Avatar';
 import Box from '@mui/joy/Box';
 import List from '@mui/joy/List';
@@ -7,29 +8,41 @@ import ListItemDecorator from '@mui/joy/ListItemDecorator';
 import Typography from '@mui/joy/Typography';
 
 const DividedList: React.FC<any> = ({ onlineUsers, onUserClick }) => {
+  const [selectedUserId, setSelectedUserId] = useState<string | null>(null);
+
+  const handleUserClick = (user: any) => {
+    setSelectedUserId(user._id);
+    onUserClick(user);
+  };
+
   return (
-    <Box
-      sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: 4 }}
-    >
-          <div>
-            <List variant="outlined" sx={{ minWidth: 240, borderRadius: 'sm' }}>
-            {onlineUsers.map((user: any, index: any) => {
-                return (
-                    <div key={user._id} id={user._id} onClick={() => onUserClick(user)}>
-                        {(index !== 0 && <ListDivider />)}
-                        <ListItem>
-                        <ListItemDecorator>
-                            <Avatar size="sm" src="/static/images/avatar/1.jpg" />
-                        </ListItemDecorator>
-                            {user.email}
-                        </ListItem>
-                    </div>
-              )
-            })}
-            </List>
-          </div>
+    <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: 4 }}>
+      <div>
+        <List variant="outlined" sx={{ minWidth: 240, borderRadius: 'sm' }}>
+          {onlineUsers.map((user: any, index: number) => (
+            <div key={user._id} id={user._id} onClick={() => handleUserClick(user)}>
+              {index !== 0 && <ListDivider />}
+              <ListItem
+                sx={{
+                  cursor: 'pointer',
+                  backgroundColor: user._id === selectedUserId ? 'var(--color-1)' : 'transparent',
+                  color: user._id === selectedUserId ? 'white' : 'inherit',
+                  borderRadius: '5px',
+                  transition: 'background 0.1s ease-in-out',
+                  '&:hover': { backgroundColor: 'var(--color-4)' },
+                }}
+              >
+                <ListItemDecorator>
+                  <Avatar size="sm" src="/static/images/avatar/1.jpg" />
+                </ListItemDecorator>
+                <Typography>{user.email}</Typography>
+              </ListItem>
+            </div>
+          ))}
+        </List>
+      </div>
     </Box>
   );
-}
+};
 
 export default DividedList;
