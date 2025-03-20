fadb80a875771b4aad649f7b5158df2df89c83af
LiavTB <49226046+LiavTB@users.noreply.github.com>
2025-03-20 17:17:21 +0200

    fix swagger and maybe fe

--- insighthub-backend/src/app.ts
+++ insighthub-backend/src/app.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response, NextFunction} from 'express';
+import express, {Request, Response, NextFunction, RequestHandler} from 'express';
 import authRoutes from './routes/auth_routes';
 import commentsRoutes from './routes/comments_routes';
 import postsRoutes from './routes/posts_routes';
@@ -44,8 +44,11 @@ app.use(removeUndefinedOrEmptyFields);
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(loadOpenApiFile() as JsonObject));
-
+app.use('/api-docs', (req, res, next) => {
+    const prefix = req.headers['x-forwarded-prefix'] || '';
+    const swaggerDoc = loadOpenApiFile(prefix);
+    swaggerUi.setup(swaggerDoc)(req, res, next);
+}, swaggerUi.serve);
 
 // Add Authentication for all routes except the ones listed below
 app.use(authenticateToken.unless({
--- insighthub-backend/src/openapi/openapi_loader.ts
+++ insighthub-backend/src/openapi/openapi_loader.ts
@@ -2,11 +2,17 @@ import yaml from 'js-yaml';
 import fs from 'fs';
 import path from 'path';
 
-const loadOpenApiFile = () => {
+const loadOpenApiFile = (prefix: string) => {
     try {
         const swaggerPath = path.join(__dirname, 'swagger.yaml');
         const swaggerContent = fs.readFileSync(swaggerPath, 'utf8');
-        return yaml.load(swaggerContent);
+        const swaggerDoc = yaml.load(swaggerContent) as object;
+
+        if (prefix) {
+            swaggerDoc.basePath = prefix;
+        }
+
+        return swaggerDoc;
     } catch (error) {
         console.error('Error loading OpenAPI file:', error);
         return error;
