57516881cb38ba9c4427afad54369785d3167f42
Tal Jacob <taljacob2@gmail.com>
2025-03-20 13:01:56 +0200

    Fix Compiling Error For `npm run build`
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- insighthub-frontend/src/config.ts
+++ insighthub-frontend/src/config.ts
@@ -1,8 +1,8 @@
 export const config = {
     app: {
-        backend_url: () => process?.env.VITE_BACKEND_URL|| import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000',
-        domain_name: () => process?.env.VITE_DOMAIN_NAME || import.meta.env.VITE_DOMAIN_NAME || 'localhost',
-        port: () => parseInt(process?.env.VITE_PORT || import.meta.env.VITE_PORT || '5000')
+        backend_url: () => process?.env.VITE_BACKEND_URL|| (import.meta as any).env?.VITE_BACKEND_URL || 'http://localhost:3000',
+        domain_name: () => process?.env.VITE_DOMAIN_NAME || (import.meta as any).env?.VITE_DOMAIN_NAME || 'localhost',
+        port: () => parseInt(process?.env.VITE_PORT || (import.meta as any).env?.VITE_PORT || '5000')
     },
     socketMethods: {
         messageFromServer: "message-from-server",
