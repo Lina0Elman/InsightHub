029bfccd244d802862239bcbe3648d89655a0b1b
lina elman <lina.elman7@gmail.com>
2025-03-22 22:34:46 +0200

    try small fix?

--- insighthub-frontend/src/pages/Chat.tsx
+++ insighthub-frontend/src/pages/Chat.tsx
@@ -18,7 +18,7 @@ const Chat: React.FC = () => {
   const messagesEndRef = useRef<HTMLDivElement | null>(null);
   
   const connectHandler = () => {
-    socketRef.current = io(`${config.app.backend_url()}/socket.io/`, {
+    socketRef.current = io(`/`, {
       extraHeaders: {
         authorization: `Bearer ${userAuthRef.current.accessToken}`
       },
